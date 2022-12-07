763ae97ed59dfb204f6930f6316ac6fc2094f99a
af-a1997 <aldo.franquez.1997@gmail.com>
2022-12-07 20:56:32 -0300

    Upload README, models and DB queries

---
+++ README.md
@@ -0,0 +1,5 @@
+# Electro Urn API
+
+A simple API for voting candidates to various different roles, made as part of a software architecture assignment, from a course involving system analysis and development offered by [IFSul](http://www.ifsul.edu.br).
+
+> :warning: the API is currently in development, documentation will be added later.
---
+++ models/urn_db_sample_data_population.sql
@@ -0,0 +1,39 @@
+-- Sample values for database testing. Email addresses are purely 
+
+INSERT INTO voters (first_name, last_name, date_birth) VALUES
+	("Aldo", "Franquez", "1997-08-29"),
+	("Another", "User", "2003-04-30")
+;
+
+/*
+	Candidate types based on the homework's specifications, IDs are as follows:
+	
+	* 0 = President
+	* 1 = Governor
+	* 2 = Senator
+	* 3 = Federal representative
+	* 4 = Intrastate representative
+*/
+INSERT INTO candidate_types (name, notes) VALUES
+	("President", "N/A"),
+	("Governor", "N/A"),
+	("Senator", "N/A"),
+	("Federal representative", "N/A"),
+	("Intrastate representative", "N/A")
+;
+
+-- Names of important people from Uruguay, I just chose random ones.
+INSERT INTO candidates (first_name, last_name, date_birth) VALUES
+	("Daniel", "Martínez", "1957-02-23"),
+	("Luis", "Lacalle Pou", "1973-08-11"),
+	("Ernesto", "Talvi", "1957-06-10"),
+	("José", "Mujica", "1935-05-20"),
+	("Beatriz", "Argimón", "1961-08-14"),
+	("Guido", "Manini Ríos", "1958-08-20")
+;
+
+-- Inserts turns 1 and 2, dates are used to control voting constraints and are updated via the HTTP requests.
+INSERT INTO turn (dt_begin, dt_end) VALUES
+	(NOW(), NULL),
+	(NULL, NULL)
+;
